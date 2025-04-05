4a668d6065e2792603b31fd9ce3997ddee5d9cce
audrey-onfroy <audrey.onfroy@inserm.fr>
2025-04-06 00:18:31 +0200

    Improve content

--- searchindex.en.js
+++ searchindex.en.js
@@ -1,112 +1,50 @@
 var relearn_searchindex = [
   {
-    "breadcrumb": "Welcome !",
-    "content": "I am currently research engineer in computational biology.\nI achieved my PhD thesis at Institut Mondor de Recherche Biomédicale (IMRB), in Créteil, France. You can read more about my project here.",
-    "description": "bonjour",
+    "breadcrumb": "Welcome \u003e  Codes",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet augue vel ex varius consectetur. Nulla eu consequat sapien, a ornare arcu. Morbi fringilla sollicitudin lectus dictum dictum. Maecenas rutrum facilisis nisi quis bibendum. Duis ut neque vitae sem vestibulum consectetur. Sed tempus commodo risus sit amet bibendum. Phasellus in dolor tempus, laoreet nibh a, porta sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac diam mi. Nulla in vestibulum mauris, et elementum mi. Nullam sagittis pellentesque efficitur. Aliquam erat volutpat. Morbi mattis fermentum orci, et tempus nunc tincidunt et. Cras tincidunt leo eu quam lobortis blandit.",
+    "description": "This is the vignette B",
     "tags": [],
-    "title": "Bonjour !",
-    "uri": "/about/index.html"
+    "title": "Vignette B",
+    "uri": "/codes/b/index.html"
   },
   {
-    "breadcrumb": "Welcome !",
-    "content": "This is a new chapter.",
-    "description": "This is a new chapter.",
+    "breadcrumb": "Welcome \u003e  Codes",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet augue vel ex varius consectetur. Nulla eu consequat sapien, a ornare arcu. Morbi fringilla sollicitudin lectus dictum dictum. Maecenas rutrum facilisis nisi quis bibendum. Duis ut neque vitae sem vestibulum consectetur. Sed tempus commodo risus sit amet bibendum. Phasellus in dolor tempus, laoreet nibh a, porta sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac diam mi. Nulla in vestibulum mauris, et elementum mi. Nullam sagittis pellentesque efficitur. Aliquam erat volutpat. Morbi mattis fermentum orci, et tempus nunc tincidunt et. Cras tincidunt leo eu quam lobortis blandit.",
+    "description": "This is the vignette C",
     "tags": [],
-    "title": "Chapitre",
-    "uri": "/vignettes/index.html"
+    "title": "Vignette C",
+    "uri": "/codes/c/index.html"
   },
   {
-    "breadcrumb": "Welcome ! \u003e  Chapitre",
+    "breadcrumb": "Welcome \u003e  Codes",
     "content": "This vignette contains an already generated HTML file, from RStudio...",
-    "description": "This vignette contains an already generated HTML file, from RStudio...",
+    "description": "This is the vignette D",
     "tags": [],
-    "title": "Include compiled Rmd file",
-    "uri": "/vignettes/d/d/index.html"
+    "title": "Include compiled HTML from Rmd",
+    "uri": "/codes/d/index.html"
   },
   {
-    "breadcrumb": "Welcome ! \u003e  Tags",
-    "content": "",
-    "description": "",
+    "breadcrumb": "Welcome \u003e  Posts",
+    "content": "I planned to build my Github pages since some time… I managed to do it ! 😀\nTo do list My To do list contains things I would like to learn how to do :\nadd HTML compiled from Rmd",
+    "description": "This is the vignette D",
     "tags": [],
-    "title": "Tag :: Tag1",
-    "uri": "/tags/tag1/index.html"
+    "title": "Welcome to my GitHub Pages !",
+    "uri": "/posts/welcome/index.html"
   },
   {
-    "breadcrumb": "Welcome ! \u003e  Tags",
-    "content": "",
-    "description": "",
+    "breadcrumb": "",
+    "content": "Hello there !\nI am currently research engineer in computational biology.\nI achieved my PhD thesis at Institut Mondor de Recherche Biomédicale (IMRB), in Créteil, France. You can read more about my project here.\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur augue nec sem tempus euismod. Maecenas et nunc quis lectus pretium semper. Mauris sed elementum arcu. Aenean sit amet urna viverra, volutpat tellus ut, semper nibh. Sed mi magna, imperdiet sed congue in, tempus at turpis. Duis non maximus tortor. Duis at auctor tellus. Mauris commodo ligula at ultrices tristique. Etiam vehicula neque urna, a elementum lorem placerat eu. Quisque quis mauris justo. Fusce dictum elit urna, ut molestie arcu lobortis malesuada. Donec mauris felis, hendrerit id dapibus faucibus, volutpat ac justo. Aliquam eget tincidunt augue, eget dictum nibh. Curabitur laoreet lobortis felis vitae pharetra. Nunc ex turpis, lobortis in lobortis vel, tempor ut leo.\nNulla rutrum iaculis urna at fermentum. Morbi suscipit justo non neque congue, ac varius urna consequat. Nunc gravida lacus vitae interdum egestas. Vestibulum quis ante non est volutpat tempor sed nec erat. Aliquam blandit quam sit amet erat varius eleifend. Suspendisse a mauris erat. In at enim interdum, rutrum diam eu, tempor sem. In et nisi in tellus maximus imperdiet. Morbi ac rutrum leo. Maecenas lacinia ligula metus, quis lacinia diam imperdiet a. Ut sit amet metus accumsan, tempor est sed, ultricies eros. Sed nec massa laoreet, malesuada tellus at, fringilla ligula.\nInteger neque libero, dignissim vel ligula ac, pulvinar tristique libero. Proin semper magna eros, sit amet varius libero lobortis nec. Nunc blandit nisl vel eros mollis commodo. Nullam finibus nibh ut arcu placerat, quis rutrum velit vehicula. Vivamus in tempor lorem. Suspendisse non rhoncus enim. In hac habitasse platea dictumst. Vivamus nec massa at diam elementum placerat id non turpis. Maecenas sit amet feugiat nibh. Suspendisse a ante sed augue hendrerit tristique eleifend vel sapien. Nunc fringilla posuere sapien eu venenatis. Etiam erat erat, pretium eget mi in, blandit fringilla elit. Morbi elementum libero ut purus lobortis vestibulum. Vestibulum id ipsum sed risus pellentesque tempus interdum eget nisl.\nPellentesque placerat sollicitudin euismod. Sed mollis aliquet risus ac auctor. Nulla eleifend nunc enim, et semper lectus finibus vel. Quisque quis ullamcorper augue, et faucibus mauris. Donec elementum purus at nibh varius molestie. Nulla at metus auctor, pharetra dolor in, consequat velit. Proin lacus arcu, volutpat nec condimentum a, dapibus sit amet turpis. Vivamus vitae ligula sed lectus consectetur efficitur. Suspendisse tempus at arcu vel elementum. Aenean fermentum luctus efficitur. Sed id scelerisque libero. Duis commodo dolor sit amet malesuada gravida.\nPhasellus ultricies eros a neque viverra, nec commodo magna maximus. Pellentesque at risus placerat, tempus dolor sit amet, commodo dolor. Nulla et sodales mi, nec vulputate nibh. Proin convallis sapien vitae vulputate feugiat. Sed pretium viverra eleifend. Vestibulum rhoncus congue lacus, eget ultrices libero pellentesque a. Quisque ante eros, bibendum ac condimentum non, interdum vel ex. Maecenas nec massa in dolor ultrices pharetra et sed sapien.\nLast update : 2025-04-05",
+    "description": "This is the vignette D",
     "tags": [],
-    "title": "Tag :: Tag2",
-    "uri": "/tags/tag2/index.html"
+    "title": "Welcome",
+    "uri": "/index.html"
   },
   {
-    "breadcrumb": "Welcome !",
+    "breadcrumb": "Welcome",
     "content": "",
     "description": "",
     "tags": [],
     "title": "Tags",
     "uri": "/tags/index.html"
-  },
-  {
-    "breadcrumb": "Welcome ! \u003e  Chapitre",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet augue vel ex varius consectetur. Nulla eu consequat sapien, a ornare arcu. Morbi fringilla sollicitudin lectus dictum dictum. Maecenas rutrum facilisis nisi quis bibendum. Duis ut neque vitae sem vestibulum consectetur. Sed tempus commodo risus sit amet bibendum. Phasellus in dolor tempus, laoreet nibh a, porta sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac diam mi. Nulla in vestibulum mauris, et elementum mi. Nullam sagittis pellentesque efficitur. Aliquam erat volutpat. Morbi mattis fermentum orci, et tempus nunc tincidunt et. Cras tincidunt leo eu quam lobortis blandit.",
-    "description": "This is the vignette A1",
-    "tags": [
-      "Tag1",
-      "Tag2"
-    ],
-    "title": "Vignette A1",
-    "uri": "/vignettes/a/vignette_a1/index.html"
-  },
-  {
-    "breadcrumb": "Welcome ! \u003e  Chapitre",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet augue vel ex varius consectetur. Nulla eu consequat sapien, a ornare arcu. Morbi fringilla sollicitudin lectus dictum dictum. Maecenas rutrum facilisis nisi quis bibendum. Duis ut neque vitae sem vestibulum consectetur. Sed tempus commodo risus sit amet bibendum. Phasellus in dolor tempus, laoreet nibh a, porta sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac diam mi. Nulla in vestibulum mauris, et elementum mi. Nullam sagittis pellentesque efficitur. Aliquam erat volutpat. Morbi mattis fermentum orci, et tempus nunc tincidunt et. Cras tincidunt leo eu quam lobortis blandit.",
-    "description": "This is the vignette A2",
-    "tags": [
-      "Tag1",
-      "Tag2"
-    ],
-    "title": "Vignette A2",
-    "uri": "/vignettes/a/vignette_a2/index.html"
-  },
-  {
-    "breadcrumb": "Welcome ! \u003e  Chapitre",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet augue vel ex varius consectetur. Nulla eu consequat sapien, a ornare arcu. Morbi fringilla sollicitudin lectus dictum dictum. Maecenas rutrum facilisis nisi quis bibendum. Duis ut neque vitae sem vestibulum consectetur. Sed tempus commodo risus sit amet bibendum. Phasellus in dolor tempus, laoreet nibh a, porta sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac diam mi. Nulla in vestibulum mauris, et elementum mi. Nullam sagittis pellentesque efficitur. Aliquam erat volutpat. Morbi mattis fermentum orci, et tempus nunc tincidunt et. Cras tincidunt leo eu quam lobortis blandit.",
-    "description": "This is the vignette B",
-    "tags": [],
-    "title": "Vignette B",
-    "uri": "/vignettes/b/b/index.html"
-  },
-  {
-    "breadcrumb": "Welcome ! \u003e  Chapitre",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet augue vel ex varius consectetur. Nulla eu consequat sapien, a ornare arcu. Morbi fringilla sollicitudin lectus dictum dictum. Maecenas rutrum facilisis nisi quis bibendum. Duis ut neque vitae sem vestibulum consectetur. Sed tempus commodo risus sit amet bibendum. Phasellus in dolor tempus, laoreet nibh a, porta sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac diam mi. Nulla in vestibulum mauris, et elementum mi. Nullam sagittis pellentesque efficitur. Aliquam erat volutpat. Morbi mattis fermentum orci, et tempus nunc tincidunt et. Cras tincidunt leo eu quam lobortis blandit.",
-    "description": "This is the vignette C",
-    "tags": [],
-    "title": "Vignette C",
-    "uri": "/vignettes/c/c/index.html"
-  },
-  {
-    "breadcrumb": "Welcome !",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "Posts",
-    "uri": "/posts/index.html"
-  },
-  {
-    "breadcrumb": "",
-    "content": "Hello there !\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur augue nec sem tempus euismod. Maecenas et nunc quis lectus pretium semper. Mauris sed elementum arcu. Aenean sit amet urna viverra, volutpat tellus ut, semper nibh. Sed mi magna, imperdiet sed congue in, tempus at turpis. Duis non maximus tortor. Duis at auctor tellus. Mauris commodo ligula at ultrices tristique. Etiam vehicula neque urna, a elementum lorem placerat eu. Quisque quis mauris justo. Fusce dictum elit urna, ut molestie arcu lobortis malesuada. Donec mauris felis, hendrerit id dapibus faucibus, volutpat ac justo. Aliquam eget tincidunt augue, eget dictum nibh. Curabitur laoreet lobortis felis vitae pharetra. Nunc ex turpis, lobortis in lobortis vel, tempor ut leo.\nNulla rutrum iaculis urna at fermentum. Morbi suscipit justo non neque congue, ac varius urna consequat. Nunc gravida lacus vitae interdum egestas. Vestibulum quis ante non est volutpat tempor sed nec erat. Aliquam blandit quam sit amet erat varius eleifend. Suspendisse a mauris erat. In at enim interdum, rutrum diam eu, tempor sem. In et nisi in tellus maximus imperdiet. Morbi ac rutrum leo. Maecenas lacinia ligula metus, quis lacinia diam imperdiet a. Ut sit amet metus accumsan, tempor est sed, ultricies eros. Sed nec massa laoreet, malesuada tellus at, fringilla ligula.\nInteger neque libero, dignissim vel ligula ac, pulvinar tristique libero. Proin semper magna eros, sit amet varius libero lobortis nec. Nunc blandit nisl vel eros mollis commodo. Nullam finibus nibh ut arcu placerat, quis rutrum velit vehicula. Vivamus in tempor lorem. Suspendisse non rhoncus enim. In hac habitasse platea dictumst. Vivamus nec massa at diam elementum placerat id non turpis. Maecenas sit amet feugiat nibh. Suspendisse a ante sed augue hendrerit tristique eleifend vel sapien. Nunc fringilla posuere sapien eu venenatis. Etiam erat erat, pretium eget mi in, blandit fringilla elit. Morbi elementum libero ut purus lobortis vestibulum. Vestibulum id ipsum sed risus pellentesque tempus interdum eget nisl.\nPellentesque placerat sollicitudin euismod. Sed mollis aliquet risus ac auctor. Nulla eleifend nunc enim, et semper lectus finibus vel. Quisque quis ullamcorper augue, et faucibus mauris. Donec elementum purus at nibh varius molestie. Nulla at metus auctor, pharetra dolor in, consequat velit. Proin lacus arcu, volutpat nec condimentum a, dapibus sit amet turpis. Vivamus vitae ligula sed lectus consectetur efficitur. Suspendisse tempus at arcu vel elementum. Aenean fermentum luctus efficitur. Sed id scelerisque libero. Duis commodo dolor sit amet malesuada gravida.\nPhasellus ultricies eros a neque viverra, nec commodo magna maximus. Pellentesque at risus placerat, tempus dolor sit amet, commodo dolor. Nulla et sodales mi, nec vulputate nibh. Proin convallis sapien vitae vulputate feugiat. Sed pretium viverra eleifend. Vestibulum rhoncus congue lacus, eget ultrices libero pellentesque a. Quisque ante eros, bibendum ac condimentum non, interdum vel ex. Maecenas nec massa in dolor ultrices pharetra et sed sapien.\nLast update : 2022-12-04",
-    "description": "Welcome",
-    "tags": [],
-    "title": "Welcome !",
-    "uri": "/index.html"
-  },
-  {
-    "breadcrumb": "Welcome ! \u003e  Posts",
-    "content": "I planned to build my Github pages since some time… I managed to do it ! 😀\nTo do list My To do list contains things I would like to learn how to do :\nadd HTML compiled from Rmd",
-    "description": "Welcome",
-    "tags": [],
-    "title": "Welcome to my GitHub pages !",
-    "uri": "/posts/welcome/index.html"
   }
 ]
